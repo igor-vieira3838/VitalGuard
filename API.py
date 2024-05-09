0420de27130344150c27285f80e75ae8438d96bd
Igor Vieira <igorvieira.inatel@gmail.com>
2024-05-08 21:20:51 -0300

    Update API.py

--- API.py
+++ API.py
@@ -1,14 +1,15 @@
 import pymongo
-from pymongo.errors import OperationFailure
-from bson import ObjectId
-from bson import errors
 import paho.mqtt.client as mqtt
 import json
 from datetime import datetime
+import uuid
 
 myclient = pymongo.MongoClient("mongodb+srv://igorvieira:vitalguard@cluster0.w3yfk85.mongodb.net/")
 client = mqtt.Client()
 """
+
+Qual DB e qual collection usar?
+
 mydoc = {
   "cpf": "123123",
   "nome": "Ana Silva",
@@ -84,63 +85,101 @@ self.mycol = self.mydb["infos"]
 
 mycol.insert_one(mydoc)
 """
-
 class MongoCall:
   def __init__(self, payload_data):
     self.payload = payload_data
-    self.mydb = myclient["teste"]
-    self.mycol = self.mydb["infos"]
   
-  #Cadastro do profissional da saúde
-  def Cadastro(self):
+  def Login(self): #Login do profissional da saúde (app=>API)
+    mydb = myclient["Users"]
+    mycol = mydb["infos"]
+    
+    try:
+        email = self.payload["email"]
+        senha = self.payload["senha"]
+    except Exception as e:
+        print("Login: " + str(e))
+    
+    #match do email e senha + retorna o campo "id_paho_mqtt"
+    #id_paho_mqtt é a id única de um usuário
+    login_check = mycol.find_one({"email": str(email), "senha": str(senha)}, {"id_paho_mqtt": True})
+    
+    if login_check:
+      id_paho_mqtt = str(login_check["id_paho_mqtt"])
+    else:
+      print("Usuário não encontrado")
+      #**id_da_sessão_app** deve ser uma id única que é utilizada somente
+      #para diferenciar um aparelho do outro no broker para que não haja 
+      #conflito na comunicação entre 2 aparelhos não logados em uma conta.
+      #Deve ser gerado no app.
+      client.publish("VitalGuard/**id_da_sessão_app**/login/status", {"aprovado": False})
+      return
+    
+    client.publish(f"VitalGuard/{id_paho_mqtt}/login/status", {"aprovado": True, "id_paho_mqtt": id_paho_mqtt})
+
+  def Cadastro(self):   #Cadastro do profissional da saúde
+    mydb = myclient["Users"]
+    mycol = mydb["infos"]
+    
     #payload exemplo (app=>API)
     """
     {
-      "nome": "asd",
-      "senha": "qwe",
-      "cpf": "123123",
-      "email": "asd@qwe.br"
+      "nome": "Igor",
+      "senha": "abc",
+      "cpf": "48858859812",
+      "email": "vitalguard@gmail.com"
     }
     """
+    
     try:
-      nome = self.payload["nome"]
-      senha = self.payload["senha"]
-      cpf = self.payload["cpf"]
-      email = self.payload["email"]
-      
-      conta_check = self.mycol.find_one({"cpf": cpf}, {"cpf": True})
-      
-      if conta_check != None:
-        print("CPF já cadastrado!")
-        return
-      
-      if(conta_check["cpf"] == cpf):
-        print("CPF já cadastrado!")
-        client.publish("Vitalguard/cadastro/**id_mqtt**/cadastroFalhou", {})
-        return
-      
-      """
-      if(conta_check["email"] == email):
-        print("Email já cadastrado!")
-        client.publish("Vitalguard/cadastro/**id_mqtt**/cadastroFalhou", {})
-        return
-      """
-      
-      myobj = {
-        "nome": str(nome),
-        "email": str(email),
-        "senha": str(senha),
-        "cpf": str(cpf)
-      }
-      
-      self.mycol.insert_one(myobj)
-    except OperationFailure as e:
+      nome = str(self.payload["nome"])
+      senha = str(self.payload["senha"])
+      cpf = str(self.payload["cpf"])
+      email = str(self.payload["email"])
+    except Exception as e:
       print("Cadastro: " + str(e))
+    
+    cpf_check = mycol.find_one({"cpf": cpf})
+    email_check = mycol.find_one({"email": email})
+    
+    #check se email ou senha já foram tomados, retorna caso os campos sejam (!= None)
+    if (cpf_check and email_check) != None:
+      print("CPF e email já cadastrado!")
+      client.publish("VitalGuard/**id_sessão**/cadastro/cadastroFalhou", "cpf&emailDuplicado")
+      return
+    elif email_check != None:
+      print("Email já cadastrado!")
+      client.publish("VitalGuard/**id_sessão**/cadastro/cadastroFalhou", "emailDuplicado")
+      return
+    elif cpf_check != None:
+      print("CPF já cadastrado!")
+      client.publish("VitalGuard/**id_sessão**/cadastro/cadastroFalhou", "cpfDuplicado")
+      return
+    else:
+      pass
+    
+    #criação de id mqtt para o novo user
+    client_id = str(uuid.uuid4())
+    
+    myobj = {
+      "nome": nome,
+      "email": email,
+      "senha": senha,
+      "cpf": cpf,
+      "id_paho_mqtt": client_id
+    }
+
+    mycol.insert_one(myobj)
+    
+    print("Documento inserido!")
   
-  def CadastroPaciente(self):
+  def CadastroPaciente(self): #cadastro de um paciente
     pass
       
-  def Update(self):
+  def Update(self): #update das infos do paciente
+      
+    mydb = myclient["Paciente"]
+    mycol = mydb["infos"]
+    
     try:
       cpf = self.payload["cpf"]
       BPM = self.payload["BPM"]
@@ -152,8 +191,8 @@ class MongoCall:
           }
       }
       
-      self.mycol.update_one({"cpf": cpf}, update_query)
-    except OperationFailure as e:
+      mycol.update_one({"cpf": cpf}, update_query)
+    except Exception as e:
       print("MongoCall.update: " + str(e))
     else:
       print("Documento atualizado!")
@@ -165,26 +204,33 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
   try:
-    payload_data = json.loads(str(msg.payload.decode()))
-    
-    # Instância que interage com o banco de dados
-    mongoDB = MongoCall(payload_data)
-    
-    if(msg.topic == "VitalGuard/cadastro/dados"):
-      mongoDB.Cadastro()
-    
-    if(msg.topic == "VitalGuard/sensor/dados"):
-      mongoDB.Update()
-            
+    payload_str = str(msg.payload.decode())
+    if payload_str:  # Verifica se o payload não está vazio
+      payload_data = json.loads(payload_str)
+      # Instância que interage com o banco de dados
+      mongoDB = MongoCall(payload_data)
+    else:
+      print("Payload vazio.")
+      return
   except Exception as e:
-    print("on_message: "+str(e))
+      print("on_message: " + str(e))
+  
+  if(msg.topic == "VitalGuard/cadastro/dados"):
+    mongoDB.Cadastro()
+  
+  if(msg.topic == "VitalGuard/sensor/dados"):
+    mongoDB.Update()
+
+  if msg.topic == "VitalGuard/login":
+    mongoDB.Login()
+    
 
 client.on_connect = on_connect
 client.on_message = on_message
 		
 try:
   client.username_pw_set("Igor", "abc")    
-  client.connect("localhost", 1883, 60)
+  client.connect("192.168.0.108", 1883, 60)
 except:
   print("Não foi possivel conectar ao MQTT...")
   print("Encerrando...")
